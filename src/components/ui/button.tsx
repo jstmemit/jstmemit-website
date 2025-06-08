47441b7fd53082d1212e329f2bcb4f9308553952
kamazra4ka <admin@kamazra4ka.nl>
2025-06-08 17:47:54 +0200

    feat(website): make a universal BaseLayout and ContentLayout, adjust button styling

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,12 +3,11 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
-import { Image } from 'astro:assets'
 import discord from "@/assets/icons/discord.svg";
 import github from "@/assets/icons/github.svg";
 
 const buttonVariants = cva(
-    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium cursor-pointer transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
     {
       variants: {
         variant: {
@@ -73,7 +72,8 @@ function Button({
       )
     }
 
-    return children
+    // @ts-ignore
+      return children
   }
 
   return (
