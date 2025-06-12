08e037d6ebaca90e4196480c5c2fd41fc91c1e9f
kamazra4ka <admin@kamazra4ka.nl>
2025-06-12 23:15:39 +0200

    feat(website): add favicon, title, description information + content compression via compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,23 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
+import node from '@astrojs/node';
+
+import playformCompress from '@playform/compress';
+
+import compressor from 'astro-compressor';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap(), playformCompress(), compressor()],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
