6b86ae8b63dc0e6dc63a05710928f93a81c8c253
kamazra4ka <admin@kamazra4ka.nl>
2025-06-12 23:49:40 +0200

    feat(website): add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,13 +12,16 @@ import playformCompress from '@playform/compress';
 
 import compressor from 'astro-compressor';
 
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
   },
 
-  integrations: [react(), sitemap(), playformCompress(), compressor()],
+  site: 'https://jstmemit.com',
+  integrations: [react(), sitemap(), playformCompress(), compressor(), robotsTxt()],
 
   adapter: node({
     mode: 'standalone',
